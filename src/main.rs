028e48ae6d95e46cc267111a144582dc4fc3d4af
Ping <54463205+Opposite34@users.noreply.github.com>
2022-12-24 19:10:11 +0900

    cleaning types

--- src/main.rs
+++ src/main.rs
@@ -19,20 +19,20 @@ const VOLUME_CTRL_SLEEP: Duration = Duration::from_millis(100);
 
 fn main() -> std::io::Result<()>{
     let(_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
+    let sink: Sink = Sink::try_new(&stream_handle).unwrap();
     
     //your music path here
-    let file = File::open("music/the_25th_hour.wav")?;
-    let buf = BufReader::new(file);
+    let file: File = File::open("music/the_25th_hour.wav")?;
+    let buf: BufReader<File> = BufReader::new(file);
 
-    let source = Decoder::new(buf).unwrap();
+    let source: Decoder<BufReader<File>> = Decoder::new(buf).unwrap();
     sink.append(source);
     
-    let device_state = DeviceState::new();
+    let device_state: DeviceState = DeviceState::new();
     
-    let mut vol = sink.volume();
-    let mut vol_key_pressed = false;
-    let mut space_hold = false;
+    let mut vol: f32 = sink.volume();
+    let mut vol_key_pressed: bool = false;
+    let mut space_hold: bool = false;
     
     while !sink.empty() {
         let keys: Vec<Keycode> = device_state.get_keys();
@@ -79,7 +79,7 @@ fn main() -> std::io::Result<()>{
         if vol_key_pressed {
             vol_key_pressed = false;
 
-            let vol_not_clamped = (vol * 100.0).round() / 100.0;
+            let vol_not_clamped: f32 = (vol * 100.0).round() / 100.0;
             vol = vol_not_clamped.clamp(VOLUME_MIN, VOLUME_MAX);
             
             if vol_not_clamped == vol {
